4d168a13617fca034d7787b27f89f14dec6310c9
fidel <lsfidel2@gmail.com>
2023-04-24 14:09:21 -0500

    Primer Ejercicio

---
+++ Ejercicio1.java
@@ -0,0 +1,9 @@
+public class Ejercicio1 {
+    public static void main(String[] args)
+    {
+        System.out.println(suma(3, 5, 8));
+    }
+    public static int suma(int a, int b, int c){
+    return a + b + c;
+    }
+}
