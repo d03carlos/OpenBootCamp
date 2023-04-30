2d64dc931f39e8c0ca67f03669751eab8a0c354a
fidel <lsfidel2@gmail.com>
2023-04-30 09:58:19 -0500

    Primer ejercicio

---
+++ miproyecto.java
@@ -0,0 +1,16 @@
+package com.miproyecto;
+public class miproyecto{
+    public  static void main(String[] args) {
+        int numero=10;
+        long num=15;
+        double doble=12.2d;
+        boolean bool = true;
+        String nombre="Juan";
+        char caracter = 'a';
+        System.out.print(bool);
+        System.out.println(numero);
+        System.out.println(num);
+        System.out.println(nombre);
+        System.out.println(caracter);
+    }
+}
